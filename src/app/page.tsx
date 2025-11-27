64cc5998e8bfe63b9338d4b56429050c9849ec02
cabad <testcabad@gmail.com>
2025-11-28 01:36:53 +0400

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import PortfolioChart from '@/components/PortfolioChart';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="p-4 lg:p-8 max-w-7xl mx-auto w-full">
-        <PortfolioChart />
-        {/* <div className="mt-8">
+    <main className="page-container">
+      <div className="grid grid-cols-1 lg:grid-cols-3">
+        <div className="col-span-2"><PortfolioChart /></div>
+      </div>
+      {/* <div className="mt-8">
           <h3 className="text-lg font-semibold text-standard mb-4">Your Assets</h3>
           <PortfolioGrid />
         </div> */}
-      </main>
-    </div>
+    </main>
   );
 }
